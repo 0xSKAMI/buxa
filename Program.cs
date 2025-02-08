f70afb83f95393a6a77dc2d607bc766df7b53e33
JSlayer93 <saba.manchkhashvili23@gmail.com>
2025-02-09 02:48:15 +0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DotNetEnv;
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+	private static async Task Main(string[] args)
+	{
+		Env.Load();
+		string token = Env.GetString("TOKEN");
+
+		var client = new DiscordSocketClient();
+		await client.LoginAsync(TokenType.Bot, token);
+		await client.StartAsync();
+		await Task.Delay(-1);
+
+		await client.Ready() += 1;
+	}
+
+	private static async Task printConnection()
+	{
+		Console.WriteLine("[CONNECTED]: server is connected");
+	}
+}
